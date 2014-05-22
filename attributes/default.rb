2f15233152c6c3dc8569ac77a4670be3f1169736
Matt Barlow <matt.barlow@rackspace.com>
2014-05-22 14:27:30 -0500

    Update attribute with github info

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['rackenv'] = 'development'
 default['rubyversion'] = '2.1.2'
+default['original_git_repo'] = 'https://github.com/knifecluster/rails-cluster'
--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 # Encoding: utf-8
-name             'rails-stack'
+name             'rails-cluster'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'none'
 license          ''
 description      'Installs/Configures rails-stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.0'
 depends          'rbenv'
 depends          'apt'
 depends          'application'
